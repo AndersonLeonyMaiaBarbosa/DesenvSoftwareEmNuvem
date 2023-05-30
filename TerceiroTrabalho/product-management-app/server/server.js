164199fe038b73fdd5f9fd4d60e3cf7a1985d9bb
Anderson <leony_maia@edu.unifor.br>
2023-05-29 21:18:12 -0300

    change access origin setando url da instancia http://localhost:5000

--- TerceiroTrabalho/product-management-app/server/server.js
+++ TerceiroTrabalho/product-management-app/server/server.js
@@ -13,7 +13,7 @@ const supabase = createClient(supabaseUrl, supabaseKey);
 
 // Configuração de CORS
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://3.142.209.75');
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5000');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   res.setHeader('Referrer-Policy', 'no-referrer');
