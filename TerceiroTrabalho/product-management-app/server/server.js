ddf901f60bfccae2348dc2e2128e898ab4722bd5
Anderson <leony_maia@edu.unifor.br>
2023-05-29 21:09:11 -0300

    configuração refer-policy

--- TerceiroTrabalho/product-management-app/server/server.js
+++ TerceiroTrabalho/product-management-app/server/server.js
@@ -16,6 +16,7 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Referrer-Policy', 'no-referrer');
   next();
 });
 
